2bcb8852eb549ce4ad7274e2b8b6de7bc6f1369b
Sohail Mohammed <130756101+mdsoh-27@users.noreply.github.com>
2025-10-30 00:40:42 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,11 +1,15 @@
-const API_BASE = 'import.meta.env.VITE_API_BASE_URL'
-//const API_BASE = 'http://127.0.0.1:5000/'
-export async function trackProduct(productName: string) {
-  const res = await fetch(`${API_BASE}/track`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ product: productName }) // 
-  })
-  if (!res.ok) throw new Error('Network error')
-  return res.json()
-}
+const API_URL = 'import.meta.env.VITE_API_URL'
+
+export const trackProduct = async (product: string) => {
+  const response = await fetch(`${API_URL}/track`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ product }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch");
+  }
+
+  return response.json();
+};
